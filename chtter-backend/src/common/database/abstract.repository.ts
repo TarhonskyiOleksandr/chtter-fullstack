c136bf761497348f3195dff3e8e984a06bd297f7
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-06-25 11:45:40 +0300

    first commit

---
+++ chtter-backend/src/common/database/abstract.repository.ts
@@ -0,0 +1,53 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractEntity } from './abstract.entity';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+
+export abstract class AbstractRepository<T extends AbstractEntity> {
+  protected readonly logger: Logger;
+
+  constructor(protected readonly model: Model<T>) {}
+
+  async create(document: Omit<T, '_id'>): Promise<T> {
+    const newDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await newDocument.save()).toObject();
+  }
+
+  async findOne(filterQuery: FilterQuery<T>): Promise<T> {
+    const document = this.model.findOne(filterQuery).lean<T>();
+
+    if (!document) {
+      this.logger.warn('Document not found', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<T>,
+    updateQuery: UpdateQuery<T>,
+  ): Promise<T> {
+    const updatedDocument = await this.model
+      .findOneAndUpdate(filterQuery, updateQuery, { new: true })
+      .lean<T>();
+
+    if (!updatedDocument) {
+      this.logger.warn('Document not found', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return updatedDocument;
+  }
+
+  async find(filterQuery: FilterQuery<T>): Promise<T[]> {
+    return await this.model.find(filterQuery).lean<T[]>();
+  }
+
+  async findOneAndDelete(filterQuery: FilterQuery<T>): Promise<T> {
+    return await this.model.findOneAndDelete(filterQuery).lean<T>();
+  }
+}
---
+++ chtter-backend/src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly users: UsersRepository) {}
+
+  create(createUserInput: CreateUserInput) {
+    return 'This action adds a new user';
+  }
+
+  async findAll() {
+    return await this.users.find({});
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserInput: UpdateUserInput) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
