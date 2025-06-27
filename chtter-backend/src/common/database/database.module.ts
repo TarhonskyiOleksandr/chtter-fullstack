faeedefd73d8aff0a1c510a3b71df56c71b2cf12
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-06-27 11:47:33 +0300

    feat: added migrations and indexing

--- chtter-backend/src/common/database/database.module.ts
+++ chtter-backend/src/common/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
+import { DbMigrationService } from './db-migration.service';
+
 @Module({
   imports: [
     MongooseModule.forRootAsync({
@@ -11,6 +13,7 @@ import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
       inject: [ConfigService],
     }),
   ],
+  providers: [DbMigrationService],
 })
 export class DatabaseModule {
   static forFeature(models: ModelDefinition[]) {
