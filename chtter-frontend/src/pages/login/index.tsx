46bcdd85bfd13d52e8bbcd1b8a41dc4f60c4d92e
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-07-02 12:25:17 +0300

    feat(client): added login

--- chtter-frontend/src/pages/login/index.tsx
+++ chtter-frontend/src/pages/login/index.tsx
@@ -1,20 +1,35 @@
 import { FormField } from '@/shared/ui';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+
 import { loginSchema, type LoginFormData } from './types';
+import { useLogin } from '@/features';
 
 const LoginPage = () => {
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
+    setError,
+    clearErrors
   } = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
   });
+  const { login } = useLogin();
+
+  const onHandleSubmit = (data: LoginFormData) => {
+    clearErrors('root');
+    login(data, setError);
+  };
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(onHandleSubmit)}>
       <h2 className="card-title mb-3">Login</h2>
+      {errors.root && (
+        <div className="alert alert-error mb-4">
+          <span>{errors.root.message}</span>
+        </div>
+      )}
       <FormField error={errors.email?.message}>
         <input 
           placeholder="Email" 
