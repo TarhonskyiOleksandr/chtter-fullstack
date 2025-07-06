202553b915d82395a7f1a3777975bb7e2a5e8997
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-07-06 11:04:27 +0300

    feat: added redirect

--- chtter-frontend/src/app/layouts/AuthLayout.tsx
+++ chtter-frontend/src/app/layouts/AuthLayout.tsx
@@ -1,12 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+import { useGetMe } from '@/entities';
 
 export const AuthLayout = () => {
+  const { data } = useGetMe();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (data?.me) navigate('/')
+  }, [data, navigate]);
+
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="card w-96 bg-base-200 shadow-xl">
         <div className="card-body">
           <Outlet />
-        </div>
+        </div> 
       </div>
     </div>
   );
