944b873343444edb074a534900a418582afbeb46
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-07-07 11:25:16 +0300

    feat: added logout and navbar

--- chtter-frontend/src/app/layouts/Protected.tsx
+++ chtter-frontend/src/app/layouts/Protected.tsx
@@ -1,12 +1,25 @@
-import { useGetMe } from '@/entities';
 import { Navigate, Outlet } from 'react-router-dom';
+
+import { useGetMe } from '@/entities';
+import { Navbar } from './Navbar';
+import { useEffect } from 'react';
+import { isAuthenticated } from '@/shared/constatnts';
 
 export const Protected = () => {
   const { data, loading } = useGetMe();
+
+  useEffect(() => {
+    if (data?.me) isAuthenticated(true);
+  }, [data]);
   
   if (loading) return <p>Loading...</p>;
   
   if (!data?.me) return <Navigate to="/login" />;
 
-  return <Outlet />;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 };
