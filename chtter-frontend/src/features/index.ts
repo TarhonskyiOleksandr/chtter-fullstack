7bdb0d31181c43064aecbb0724977ef838bbe1f1
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-07-15 10:41:09 +0300

    fix: types hotfixes

--- chtter-frontend/src/features/index.ts
+++ chtter-frontend/src/features/index.ts
@@ -1,8 +1,5 @@
 export { useCreateUser } from './create-user/hooks/useCreateUser';
 export { useLogin } from './login/hooks/useLogin';
 export { useLogout } from './logout/hooks/useLogout';
-<<<<<<< HEAD
 export { useCreateChat } from './create-chat/hooks/useCreateChat';
-=======
 export { useGetChats } from './get-chats/hooks/useGetChats';
->>>>>>> dccfe20 (feat: added get all chats query)
