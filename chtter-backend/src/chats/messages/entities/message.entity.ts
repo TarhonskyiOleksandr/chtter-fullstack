0957507ad778e353512ca1714473a1b46447a5ed
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-07-21 11:20:31 +0300

    feat(backend): added messages module

---
+++ chtter-backend/src/chats/messages/entities/message.entity.ts
@@ -0,0 +1,22 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { AbstractEntity } from 'src/common/database/abstract.entity';
+
+@ObjectType()
+@Schema()
+export class Message extends AbstractEntity {
+  @Field()
+  @Prop()
+  content: string;
+
+  @Field()
+  @Prop()
+  userId: string;
+
+  @Field()
+  @Prop()
+  createdAt: Date;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
