c17e93dad97ade5e8bc4ad4ad7c23fdbc043e2b4
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-07-30 11:45:32 +0300

    fix: removed pubSub from messages.resolver

--- chtter-backend/src/chats/messages/messages.resolver.ts
+++ chtter-backend/src/chats/messages/messages.resolver.ts
@@ -1,23 +1,18 @@
 import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
-import { PubSub } from 'graphql-subscriptions';
 
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { Inject, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { JWTPayload } from 'src/auth/types/jwt-payload.interface';
 import { GetMessagesArgs } from './dto/get-messages.args';
-import { PUB_SUB } from 'src/common/injection-tokens';
 import { MessageCreatedArgs } from './dto/message-created.args';
 
 @Resolver()
 export class MessagesResolver {
-  constructor(
-    private readonly messagesService: MessagesService,
-    @Inject(PUB_SUB) private readonly pubSub: PubSub,
-  ) {}
+  constructor(private readonly messagesService: MessagesService) {}
 
   @Mutation(() => Message)
   @UseGuards(GqlAuthGuard)
