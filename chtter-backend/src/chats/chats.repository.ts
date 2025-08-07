4f401c63cf24db0e85f88f0c6f309373bdb1f3ad
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-08-07 20:15:21 +0300

    refactor(backend): messages aggregation

--- chtter-backend/src/chats/chats.repository.ts
+++ chtter-backend/src/chats/chats.repository.ts
@@ -3,13 +3,13 @@ import { Model } from 'mongoose';
 import { Injectable, Logger } from '@nestjs/common';
 
 import { AbstractRepository } from 'src/common/database/abstract.repository';
-import { Chat } from './entities/chat.entity';
+import { ChatDocument } from './entities/chat.document';
 
 @Injectable()
-export class ChatsRepository extends AbstractRepository<Chat> {
+export class ChatsRepository extends AbstractRepository<ChatDocument> {
   protected readonly logger = new Logger(ChatsRepository.name);
 
-  constructor(@InjectModel(Chat.name) chatModel: Model<Chat>) {
+  constructor(@InjectModel(ChatDocument.name) chatModel: Model<ChatDocument>) {
     super(chatModel);
   }
 }
