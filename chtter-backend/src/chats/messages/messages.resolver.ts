e8d8bc5e6706d9463165d949d31a25b8f8ee8d46
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-08-25 12:52:42 +0300

    fix: fixed pubSub condition

--- chtter-backend/src/chats/messages/messages.resolver.ts
+++ chtter-backend/src/chats/messages/messages.resolver.ts
@@ -36,7 +36,10 @@ export class MessagesResolver {
       const userId = context.req.user._id;
       const message: Message = payload.messageCreated;
 
-      return message.chatId === variables.chatId && userId !== message.user._id;
+      return (
+        message.chatId === variables.chatId &&
+        userId !== message.user._id.toHexString()
+      );
     },
   })
   messageCreated(@Args() messageCreatedArgs: MessageCreatedArgs) {
