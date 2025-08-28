2c0e06219224e7cbde1eaff1f4f9a41e7a050742
OleksandrTarhonskyi <oleksandr@digisol.agency>
2025-08-28 12:58:41 +0300

    feat(client): added chats sorting

--- chtter-frontend/src/app/layouts/components/ChatSidebar.tsx
+++ chtter-frontend/src/app/layouts/components/ChatSidebar.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import { CreateChatModal, useGetChats, useSendMessage } from '@/features';
 import ChatListItem from './ChatListItem';
-import { useParams } from 'react-router-dom';
+import type { Chat } from '@/shared/api/graphql/gql/graphql';
 
 interface ChatSidebarProps {
   onSelectChat: (val: any) => void;
 }
+
+const sortChats = (chatA: Chat, chatB: Chat) => {
+  if (!chatA.latestMessage) return -1;
+  const chatACreatedAt = new Date(chatA.latestMessage.createdAt).getTime();
+  const chatBCreatedAt = new Date(chatB.latestMessage?.createdAt).getTime();
+
+  return chatBCreatedAt - chatACreatedAt;
+};
 
 const ChatSidebar = ({ onSelectChat }: ChatSidebarProps) => {
   const { data } = useGetChats();
@@ -31,14 +40,14 @@ const ChatSidebar = ({ onSelectChat }: ChatSidebarProps) => {
       </div>
 
       <div className="flex-1 overflow-y-auto">
-        {data?.chats.map((chat) => (
+        {data?.chats ? [...data.chats].sort(sortChats).map((chat) => (
           <ChatListItem
             key={chat._id}
             chat={chat}
             onClick={onSelectChat}
             isActive={id === chat._id}
           />
-        ))}
+        )) : null}
       </div>
 
       <CreateChatModal 
